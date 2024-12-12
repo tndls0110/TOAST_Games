766aab762e62b18b4301d61b3b29daa9ded8a28a
kyh <fucknaber0720@naver.com>
2024-12-12 15:17:17 +0900

    no message

--- src/main/java/com/toast/management/controller/EmployeeController.java
+++ src/main/java/com/toast/management/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import com.toast.management.service.EmployeeService;
 @Controller
 public class EmployeeController {
 	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired PasswordEncoder encoder;
 	
 	private final EmployeeService employeeService;
 	
@@ -29,6 +33,12 @@ public class EmployeeController {
 	
 	@PostMapping(value="/employeeAdd.do")
 	public String employeeAddDo(@RequestParam Map<String,String> param) {
+		
+		String pw = param.get("empl_pw");
+		
+		String encodepw = encoder.encode(pw);
+		
+		param.put("empl_pw", encodepw);
 		
 		return "";
 	}
--- src/main/java/com/toast/management/dao/EmployeeDAO.java
+++ src/main/java/com/toast/management/dao/EmployeeDAO.java
@@ -1,8 +1,12 @@
 package com.toast.management.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface EmployeeDAO {
+
+	int employeeAdd(Map<String, String> param);
 
 }
