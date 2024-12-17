24b5d5e2d975ec9a502376437a9a91764475292e
Han,BeomHee <bumhihan@naver.com>
2024-12-17 09:36:21 +0900

    로그인 관련

--- src/main/java/com/toast/member/dto/MemberDTO.java
+++ src/main/java/com/toast/member/dto/MemberDTO.java
@@ -1,5 +1,244 @@
 package com.toast.member.dto;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class MemberDTO {
 
+	private int empl_idx;
+	private String empl_name; 
+	private String empl_id;
+	private int empl_gender;
+	private String empl_birth;
+	private String empl_ssn1;
+	private String empl_ssn2;
+	private String empl_account;
+	private String empl_cmp_email;
+	private String empl_cmp_phone;
+	private String empl_per_email;
+	private String empl_per_phone;
+	private String empl_addr;
+	private int empl_emp_state;
+	private Date empl_join_date;
+	private Date empl_resig_date;
+	private String empl_job;
+	private String empl_stamp;
+	private String empl_profile; 
+	private int bank_idx;
+	private int empl_changepw;
+	private String file_key;
+	private int dept_idx;
+	private int position_idx;
+	private String dept_name;
+	private String duty_name;
+	private String position_name;
+	private int duty_idx;
+	private String bank_name;
+	private int appo_idx;
+	private int appo_empl_idx;
+	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
+	private Date movein_date;
+	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
+	private Date transfer_date;
+	
+	public int getAppo_idx() {
+		return appo_idx;
+	}
+	public void setAppo_idx(int appo_idx) {
+		this.appo_idx = appo_idx;
+	}
+	public int getAppo_empl_idx() {
+		return appo_empl_idx;
+	}
+	public void setAppo_empl_idx(int appo_empl_idx) {
+		this.appo_empl_idx = appo_empl_idx;
+	}
+	public Date getMovein_date() {
+		return movein_date;
+	}
+	public void setMovein_date(Date movein_date) {
+		this.movein_date = movein_date;
+	}
+	public Date getTransfer_date() {
+		return transfer_date;
+	}
+	public void setTransfer_date(Date transfer_date) {
+		this.transfer_date = transfer_date;
+	}
+	public String getBank_name() {
+		return bank_name;
+	}
+	public void setBank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	public String getDept_name() {
+		return dept_name;
+	}
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+	public String getDuty_name() {
+		return duty_name;
+	}
+	public void setDuty_name(String duty_name) {
+		this.duty_name = duty_name;
+	}
+	public String getPosition_name() {
+		return position_name;
+	}
+	public void setPosition_name(String position_name) {
+		this.position_name = position_name;
+	}
+	public int getDept_idx() {
+		return dept_idx;
+	}
+	public void setDept_idx(int dept_idx) {
+		this.dept_idx = dept_idx;
+	}
+	public int getPosition_idx() {
+		return position_idx;
+	}
+	public void setPosition_idx(int position_idx) {
+		this.position_idx = position_idx;
+	}
+	public int getDuty_idx() {
+		return duty_idx;
+	}
+	public void setDuty_idx(int duty_idx) {
+		this.duty_idx = duty_idx;
+	}
+	public int getEmpl_idx() {
+		return empl_idx;
+	}
+	public void setEmpl_idx(int empl_idx) {
+		this.empl_idx = empl_idx;
+	}
+	public String getEmpl_name() {
+		return empl_name;
+	}
+	public void setEmpl_name(String empl_name) {
+		this.empl_name = empl_name;
+	}
+	public String getEmpl_id() {
+		return empl_id;
+	}
+	public void setEmpl_id(String empl_id) {
+		this.empl_id = empl_id;
+	}
+	public int getEmpl_gender() {
+		return empl_gender;
+	}
+	public void setEmpl_gender(int empl_gender) {
+		this.empl_gender = empl_gender;
+	}
+	public String getEmpl_birth() {
+		return empl_birth;
+	}
+	public void setEmpl_birth(String empl_birth) {
+		this.empl_birth = empl_birth;
+	}
+	public String getEmpl_ssn1() {
+		return empl_ssn1;
+	}
+	public void setEmpl_ssn1(String empl_ssn1) {
+		this.empl_ssn1 = empl_ssn1;
+	}
+	public String getEmpl_ssn2() {
+		return empl_ssn2;
+	}
+	public void setEmpl_ssn2(String empl_ssn2) {
+		this.empl_ssn2 = empl_ssn2;
+	}
+	public String getEmpl_account() {
+		return empl_account;
+	}
+	public void setEmpl_account(String empl_account) {
+		this.empl_account = empl_account;
+	}
+	public String getEmpl_cmp_email() {
+		return empl_cmp_email;
+	}
+	public void setEmpl_cmp_email(String empl_cmp_email) {
+		this.empl_cmp_email = empl_cmp_email;
+	}
+	public String getEmpl_cmp_phone() {
+		return empl_cmp_phone;
+	}
+	public void setEmpl_cmp_phone(String empl_cmp_phone) {
+		this.empl_cmp_phone = empl_cmp_phone;
+	}
+	public String getEmpl_per_email() {
+		return empl_per_email;
+	}
+	public void setEmpl_per_email(String empl_per_email) {
+		this.empl_per_email = empl_per_email;
+	}
+	public String getEmpl_per_phone() {
+		return empl_per_phone;
+	}
+	public void setEmpl_per_phone(String empl_per_phone) {
+		this.empl_per_phone = empl_per_phone;
+	}
+	public String getEmpl_addr() {
+		return empl_addr;
+	}
+	public void setEmpl_addr(String empl_addr) {
+		this.empl_addr = empl_addr;
+	}
+	public int getEmpl_emp_state() {
+		return empl_emp_state;
+	}
+	public void setEmpl_emp_state(int empl_emp_state) {
+		this.empl_emp_state = empl_emp_state;
+	}
+	public Date getEmpl_join_date() {
+		return empl_join_date;
+	}
+	public void setEmpl_join_date(Date empl_join_date) {
+		this.empl_join_date = empl_join_date;
+	}
+	public Date getEmpl_resig_date() {
+		return empl_resig_date;
+	}
+	public void setEmpl_resig_date(Date empl_resig_date) {
+		this.empl_resig_date = empl_resig_date;
+	}
+	public String getEmpl_job() {
+		return empl_job;
+	}
+	public void setEmpl_job(String empl_job) {
+		this.empl_job = empl_job;
+	}
+	public String getEmpl_stamp() {
+		return empl_stamp;
+	}
+	public void setEmpl_stamp(String empl_stamp) {
+		this.empl_stamp = empl_stamp;
+	}
+	public String getEmpl_profile() {
+		return empl_profile;
+	}
+	public void setEmpl_profile(String empl_profile) {
+		this.empl_profile = empl_profile;
+	}
+	public int getBank_idx() {
+		return bank_idx;
+	}
+	public void setBank_idx(int bank_idx) {
+		this.bank_idx = bank_idx;
+	}
+	public int getEmpl_changepw() {
+		return empl_changepw;
+	}
+	public void setEmpl_changepw(int empl_changepw) {
+		this.empl_changepw = empl_changepw;
+	}
+	public String getFile_key() {
+		return file_key;
+	}
+	public void setFile_key(String file_key) {
+		this.file_key = file_key;
+	}
+	
 }
